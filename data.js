a8f31fdd32dde0f5bf6419a79d34b1832cbce52f
chibuzornworie85 <chibuzornworie85@gmail.com>
2023-01-01 19:15:09 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'musics/Kayumba_-_Baisho_I_Love_You_Baby_.mp3',
+        artist: 'artist 1',
+        cover: 'dip/music pics'
+    },
+    {
+        name: 'song 2',
+        path: 'musics/Kayumba_-_Baisho_I_Love_You_Baby_.mp3',
+        artist: 'artist 2',
+        cover: 'dip/music pics'
+    },
+    {
+        name: 'song 3',
+        path: 'musics/Kayumba_-_Baisho_I_Love_You_Baby_.mp3',
+        artist: 'artist 3',
+        cover: 'dip/music pics'
+    },
+    {
+        name: 'song 4',
+        path: 'musics/Kayumba_-_Baisho_I_Love_You_Baby_.mp3',
+        artist: 'artist 4',
+        cover: 'dip/music pics'
+    },
+    {
+        name: 'song 5',
+        path: 'musics/Kayumba_-_Baisho_I_Love_You_Baby_.mp3',
+        artist: 'artist 5',
+        cover: 'dip/music pics'
+    }
+]
